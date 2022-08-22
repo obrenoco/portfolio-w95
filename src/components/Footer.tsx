3efbbd3f59ad40f3b54689d7cbd65d18f89b41c9
Breno Romeiro <bsromeiro@gmail.com>
2022-08-22 13:05:55 -0300

    add text to modal

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,25 +4,25 @@ import SoundIcon from "../assets/icons/sound.png";
 const currentTime = new Date().toLocaleTimeString('pt-BR', { hour: 'numeric', minute: 'numeric', hour12: true })
 
 type FooterType = {
-    isMenuOpen: boolean,
-    setIsMenuOpen: React.Dispatch<React.SetStateAction<boolean>>
+	isMenuOpen: boolean,
+	setIsMenuOpen: React.Dispatch<React.SetStateAction<boolean>>
 }
 export const Footer = ({ isMenuOpen, setIsMenuOpen }: FooterType) => {
 
-    return (
-        <footer className='fixed bottom-0 bg-gray-light border-t-white border-2 w-full text-black p-1 '>
-            <div className={`fixed bottom-12 left-0 bg-gray border-b-black border-r-black border-2 w-[400px] h-[600px] z-10 ${isMenuOpen ? "" : "hidden"}`}>
-            </div>
-            <div className='flex items-center justify-between w-full'>
-                <button className='flex items-center gap-2 h-fit border-2 border-r-black border-b-black px-2 py-1 font-bold' onClick={() => setIsMenuOpen(!isMenuOpen)}>
-                    <img src={WindowsIcon} alt="Windows button" />
-                    <span>Start</span>
-                </button>
-                <div className='flex gap-3 bg-gray p-1 px-2 rounded-sm border-2 border-l-gray-dark border-t-gray-dark '>
-                    <img src={SoundIcon} alt="" />
-                    <span>{currentTime}</span>
-                </div>
-            </div>
-        </footer>
-    )
+	return (
+		<footer className='fixed bottom-0 bg-gray-light border-t-white border-2 w-full text-black p-1 '>
+			<div className={`fixed bottom-12 left-0 bg-gray border-b-black border-r-black border-2 w-[400px] h-[600px] z-50 ${isMenuOpen ? "" : "hidden"}`}>
+			</div>
+			<div className='flex items-center justify-between w-full'>
+				<button className='flex items-center gap-2 h-fit border-2 border-r-black border-b-black px-2 py-1 font-bold' onClick={() => setIsMenuOpen(!isMenuOpen)}>
+					<img src={WindowsIcon} alt="Windows button" />
+					<span>Start</span>
+				</button>
+				<div className='flex gap-3 bg-gray p-1 px-2 rounded-sm border-2 border-l-gray-dark border-t-gray-dark '>
+					<img src={SoundIcon} alt="" />
+					<span>{currentTime}</span>
+				</div>
+			</div>
+		</footer>
+	)
 }
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -5,94 +5,70 @@ import MinimizeIcon from "../assets/icons/minimize-icon.png"
 import MyComputerIcon from "../assets/icons/my-computer.png"
 
 export const Modal = () => {
-    return (
-        <Draggable>
-            <div className='w-full max-w-4xl z-10 py-0.5 bg-gray border-white border-2 text-black'>
-                <header className='bg-gray-dark flex justify-between px-2 py-1'>
-                    <div className='flex items-center gap-1'>
-                        <img src={MyComputerIcon} alt="" />
-                        <h2 className='text-gray-300 font-bold'>My Portfolio</h2>
-                    </div>
-                    <ul className='list-none flex items-center gap-1'>
-                        <li className='flex'>
-                            <button>
-                                <img src={MinimizeIcon} alt="" />
-                            </button>
-                        </li>
-                        <li className='flex'>
-                            <button>
-                                <img src={MaximizeIcon} alt="" />
-                            </button>
-                        </li>
-                        <li className='flex'>
-                            <button>
-                                <img src={CloseIcon} alt="" />
-                            </button>
-                        </li>
-                    </ul>
-                </header>
+	return (
+		<Draggable>
+			<div className='w-full max-w-4xl z-10 py-0.5 bg-gray border-white border-2 text-black'>
+				<header className='bg-gray-dark flex justify-between px-2 py-1'>
+					<div className='flex items-center gap-1'>
+						<img src={MyComputerIcon} alt="" />
+						<h2 className='text-gray-300 font-bold'>My Portfolio</h2>
+					</div>
+					<ul className='list-none flex items-center gap-1'>
+						<li className='flex'>
+							<button>
+								<img src={MinimizeIcon} alt="" />
+							</button>
+						</li>
+						<li className='flex'>
+							<button>
+								<img src={MaximizeIcon} alt="" />
+							</button>
+						</li>
+						<li className='flex'>
+							<button>
+								<img src={CloseIcon} alt="" />
+							</button>
+						</li>
+					</ul>
+				</header>
 
-                <div>
-                    <ul className='flex'>
-                        <li className='px-4 hover:bg-blue-500 hover:text-white transition-colors'>File</li>
-                        <li className='px-4 hover:bg-blue-500 hover:text-white transition-colors'>File</li>
-                        <li className='px-4 hover:bg-blue-500 hover:text-white transition-colors'>File</li>
-                        <li className='px-4 hover:bg-blue-500 hover:text-white transition-colors'>File</li>
-                    </ul>
-                </div>
-                <section className='bg-white overflow-auto h-60 border-l-black border-t-black border-2 p-2'>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                    <p>This is my content</p>
-                </section>
-                <footer className='bg-gray grid grid-cols-2'>
-                    <div className=' border-black border-1 bg-gray px-2'>8 object(s)</div>
-                    <div className='border-black border-1 border-l-0 bg-gray px-2'></div>
-                </footer>
-            </div>
-        </Draggable>
-    )
+				<div>
+					<ul className='flex'>
+						<li className='px-4 hover:bg-blue-500 hover:text-white transition-colors'>File</li>
+						<li className='px-4 hover:bg-blue-500 hover:text-white transition-colors'>File</li>
+						<li className='px-4 hover:bg-blue-500 hover:text-white transition-colors'>File</li>
+						<li className='px-4 hover:bg-blue-500 hover:text-white transition-colors'>File</li>
+					</ul>
+				</div>
+				<section className='bg-white overflow-auto h-full max-h-[60vh] border-l-black border-t-black border-2 p-2'>
+					<h1>Breno Romeiro</h1>
+					<p>Hello, I'm Breno Romeiro</p>
+					<p>I'm a multilingual Front-end developer with 3 years of proven experience in helping companies create and maintain amazing software. Passionate about learning and developing with a desire to apply skills on real world challenges.</p>
+					<p className='py-2 font-bold'>My current stack:</p>
+					<ul className='list-disc list-inside'>
+						<li>HTML</li>
+						<li>CSS</li>
+						<li>Javascript | TypeScript</li>
+						<li>React</li>
+						<li>Greensock/GSAP (animation)</li>
+						<li>Cypress.io</li>
+						<li>Browser API's</li>
+					</ul>
+					<p className='py-2 font-bold'>Skills:</p>
+					<ul className='list-disc list-inside'>
+						<li className='list-disc'>Mobile First</li>
+						<li>User Experience</li>
+						<li>Agile</li>
+					</ul>
+					bsromeiro@gmail.com
+					𝘽𝙧𝙚𝙣𝙤
+
+				</section>
+				<footer className='bg-gray grid grid-cols-2'>
+					<div className=' border-black border-1 bg-gray px-2'>8 object(s)</div>
+					<div className='border-black border-1 border-l-0 bg-gray px-2'></div>
+				</footer>
+			</div>
+		</Draggable>
+	)
 }
